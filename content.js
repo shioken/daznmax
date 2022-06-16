eeeb0b632b219b089c911f4e5da376757aee518b
shioken <kasehonitan@gmail.com>
2022-06-16 20:54:57 +0900

    remove aside tag

--- content.js
+++ content.js
@@ -2,6 +2,9 @@ const applyShowPanel = () => {
     chrome.storage.local.get("maximized", function (value) {
         maintag = document.getElementsByTagName('main');
         maintag[0].style["maxWidth"] = "100%";
+
+        var e = document.getElementsByClassName("main__player-aside--23JGc")        
+        e[0].remove();
     });
 }
 
